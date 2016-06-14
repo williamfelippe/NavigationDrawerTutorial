a2bbda91b5216d9bed066f92b3d7f0ff70f9a3d1
William Felippe F. da Silva <wfelippesilva@gmail.com>
2016-06-13 22:25:23 -0300

    Menu lateral completo para o tutorial no blog do Imobilis

--- app/src/main/java/com/navigationdrawertutorial/MainActivity.java
+++ app/src/main/java/com/navigationdrawertutorial/MainActivity.java
@@ -1,15 +1,143 @@
 package com.navigationdrawertutorial;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener
 {
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+
+    private TextView username;
+    private TextView email;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initNavigationDrawer();
+        initNavigationDrawerHeader();
+        initDrawerListener(savedInstanceState);
+    }
+
+    private void initNavigationDrawer()
+    {
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        navigationView = (NavigationView) findViewById(R.id.navigation_view);
+
+        if (navigationView != null)
+        {
+            navigationView.setNavigationItemSelectedListener(this);
+        }
+    }
+
+    private void initNavigationDrawerHeader()
+    {
+        View header = navigationView.getHeaderView(0);
+        username = (TextView) header.findViewById(R.id.username);
+        email = (TextView) header.findViewById(R.id.email);
+
+        setupUserInformations();
+    }
+
+    private void setupUserInformations()
+    {
+        username.setText(R.string.test);
+        email.setText(R.string.test_email);
+    }
+
+    private void initDrawerListener(Bundle savedInstanceState)
+    {
+        if (savedInstanceState == null)
+        {
+            MenuItem item = navigationView.getMenu().getItem(0);
+            onNavigationItemSelected(item);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch (item.getItemId())
+        {
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed()
+    {
+        if (isNavigationDrawerOpen())
+        {
+            closeNavigationDrawer();
+        }
+        else
+        {
+            super.onBackPressed();
+        }
+    }
+
+    protected boolean isNavigationDrawerOpen()
+    {
+        return drawerLayout != null && drawerLayout.isDrawerOpen(GravityCompat.START);
+    }
+
+    protected void closeNavigationDrawer()
+    {
+        if (drawerLayout != null)
+        {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item)
+    {
+        item.setChecked(true);
+        drawerLayout.closeDrawers();
+        selectDrawerItem(item);
+
+        return true;
+    }
+
+    public void selectDrawerItem(MenuItem menuItem)
+    {
+        Fragment fragment = null;
+
+        switch (menuItem.getItemId())
+        {
+            case R.id.fragment1:
+                fragment = new FirstFragment();
+                break;
+
+            case R.id.fragment2:
+                fragment = new SecondFragment();
+                break;
+
+            default:
+                break;
+        }
+
+        if(fragment != null)
+        {
+            FragmentManager fragmentManager = getFragmentManager();
+            fragmentManager.beginTransaction().replace(R.id.drawer_content, fragment).commit();
+
+            setTitle(menuItem.getTitle());
+        }
     }
 }
---
+++ app/src/main/java/com/navigationdrawertutorial/SecondFragment.java
@@ -0,0 +1,43 @@
+package com.navigationdrawertutorial;
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class SecondFragment extends Fragment
+{
+    public SecondFragment()
+    {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        View view = inflater.inflate(R.layout.fragment_second, container, false);
+
+        setupToolbar(view);
+        return view;
+    }
+
+    private void setupToolbar(View view)
+    {
+        Toolbar toolbar = (Toolbar) view.findViewById(R.id.toolbar);
+
+        AppCompatActivity activity = (AppCompatActivity) getActivity();
+        activity.setSupportActionBar(toolbar);
+
+        final ActionBar bar = activity.getSupportActionBar();
+        if (bar != null)
+        {
+            bar.setDisplayHomeAsUpEnabled(true);
+            bar.setShowHideAnimationEnabled(true);
+            bar.setHomeAsUpIndicator(R.drawable.ic_navigation_menu);
+            bar.setTitle(activity.getString(R.string.fragment_2));
+        }
+    }
+}
